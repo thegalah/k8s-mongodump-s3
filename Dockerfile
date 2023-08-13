c66b642d21e1dd15f46e7a18148e79db09380b71
Matthew <thegalah@gmail.com>
2023-08-12 23:28:11 -0700

    wip

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM mongo:latest
+
+# Install AWS CLI
+RUN apt-get update && \
+    apt-get install -y python3-pip && \
+    pip3 install --upgrade pip && \
+    pip3 install awscli
+
+# Copy the script
+COPY mongodump.sh /mongodump.sh
+RUN chmod +x /mongodump.sh
+
+# Command to run the script
+ENTRYPOINT ["/mongodump.sh"]
---
+++ mongodump.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Checking required environment variables
+if [ -z "$MONGO_CONNECTION_STRING" ] || ([ -z "$MINIO_ENDPOINT" ] && [ -z "$S3_ENDPOINT" ]) || [ -z "$STORAGE_PATH" ]; then
+    echo "Missing required environment variables!"
+    exit 1
+fi
+
+# Mongodump
+mongodump --uri="$MONGO_CONNECTION_STRING" --archive="/dump/archive"
+
+# Configure AWS CLI with S3 or MinIO endpoint
+if [ -n "$S3_ENDPOINT" ]; then
+    aws configure set default.s3.endpoint_url "$S3_ENDPOINT"
+else
+    aws configure set default.s3.endpoint_url "$MINIO_ENDPOINT"
+fi
+
+# Upload to the given STORAGE_PATH
+aws s3 cp /dump/archive s3://$STORAGE_PATH
