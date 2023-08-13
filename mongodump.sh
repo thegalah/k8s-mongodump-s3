fb0bfac484e4a1df88fdf09c46d6410524c19a6e
Matthew <thegalah@gmail.com>
2023-08-13 02:02:38 -0700

    Update mongodump.sh

--- mongodump.sh
+++ mongodump.sh
@@ -12,40 +12,17 @@ mkdir -p /dump/archive
 # Building the file name with date
 FILENAME="/dump/archive/dump_$(date +'%Y%m%d%H%M').gz"
 
-# Building the mongodump command
-DUMP_OPTIONS=(--uri="$MONGO_CONNECTION_STRING" --archive="$FILENAME" --gzip --forceTableScan)
-
-if [ -n "$MONGO_USERNAME" ]; then
-    DUMP_OPTIONS+=(--username="$MONGO_USERNAME")
-fi
-
-if [ -n "$MONGO_PASSWORD" ]; then
-    DUMP_OPTIONS+=(--password="$MONGO_PASSWORD")
-fi
-
-if [ -n "$MONGO_DATABASE" ]; then
-    DUMP_OPTIONS+=(--db="$MONGO_DATABASE")
-fi
-
-if [ -n "$MONGO_COLLECTION" ]; then
-    DUMP_OPTIONS+=(--collection="$MONGO_COLLECTION")
-fi
-
-if [ -n "$MONGO_QUERY" ]; then
-    DUMP_OPTIONS+=(--query="$MONGO_QUERY")
-fi
-
 # Running the mongodump command
-mongodump "${DUMP_OPTIONS[@]}"
+mongodump --uri="$MONGO_CONNECTION_STRING" --archive="$FILENAME" --gzip --forceTableScan
 
-# Conditionally configure AWS CLI with access and secret keys
+# Configure AWS CLI with S3 or MinIO endpoint and optionally access keys
+aws configure set default.s3.endpoint_url "$S3_OR_MINIO_ENDPOINT"
 if [ -n "$ACCESS_KEY" ] && [ -n "$SECRET_KEY" ]; then
     aws configure set aws_access_key_id "$ACCESS_KEY"
     aws configure set aws_secret_access_key "$SECRET_KEY"
+else
+    NO_SIGN_REQUEST="--no-sign-request"
 fi
 
-# Configure the endpoint
-aws configure set default.s3.endpoint_url "$S3_OR_MINIO_ENDPOINT"
-
 # Upload to the given STORAGE_PATH
-aws s3 cp "$FILENAME" s3://$STORAGE_PATH
+aws s3 cp $NO_SIGN_REQUEST "$FILENAME" s3://$STORAGE_PATH --endpoint-url "$S3_OR_MINIO_ENDPOINT"
