0e77927ca7d2888592f89e054bfa7016885884b7
Matthew <thegalah@gmail.com>
2023-08-13 02:21:34 -0700

    Feature: anonymous credentials + improved readme + gzip (#2)
    * Update mongodump.sh
    
    * Update mongodump.sh
    
    * Update mongodump.sh
    
    * Update mongodump.sh
    
    * Update mongodump.sh
    
    * Update mongodump.sh
    
    * wip

--- mongodump.sh
+++ mongodump.sh
@@ -1,43 +1,50 @@
 #!/bin/bash
 
 # Checking required environment variables
-if [ -z "$MONGO_CONNECTION_STRING" ] || ([ -z "$MINIO_ENDPOINT" ] && [ -z "$S3_ENDPOINT" ]) || [ -z "$STORAGE_PATH" ]; then
+if [ -z "$MONGO_CONNECTION_STRING" ] || [ -z "$S3_OR_MINIO_ENDPOINT" ] || [ -z "$STORAGE_PATH" ]; then
     echo "Missing required environment variables!"
     exit 1
 fi
 
-# Building the mongodump command
-DUMP_CMD="mongodump --uri=\"$MONGO_CONNECTION_STRING\" --archive=\"/dump/archive\""
+# Setting the prefix for the dump files
+DUMP_PREFIX=${DUMP_PREFIX:-dump}
 
-if [ -n "$MONGO_USERNAME" ]; then
-    DUMP_CMD+=" --username=\"$MONGO_USERNAME\""
-fi
+# Creating the output directory
+mkdir -p /dump/archive
 
-if [ -n "$MONGO_PASSWORD" ]; then
-    DUMP_CMD+=" --password=\"$MONGO_PASSWORD\""
-fi
+# Building the file name with date
+FILENAME="/dump/archive/${DUMP_PREFIX}_$(date +'%Y%m%d%H%M').gz"
 
-if [ -n "$MONGO_DATABASE" ]; then
-    DUMP_CMD+=" --db=\"$MONGO_DATABASE\""
-fi
+# Function to run mongodump command
+run_mongodump() {
+    mongodump --uri="$MONGO_CONNECTION_STRING" --archive="$FILENAME" --gzip --forceTableScan
+}
 
-if [ -n "$MONGO_COLLECTION" ]; then
-    DUMP_CMD+=" --collection=\"$MONGO_COLLECTION\""
-fi
+# Attempt to run the mongodump command up to 3 times if it fails
+RETRIES=3
+for i in $(seq 1 $RETRIES); do
+    run_mongodump
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -eq 0 ]; then
+        break
+    else
+        echo "mongodump failed, attempt $i of $RETRIES"
+        rm -f "$FILENAME" # Removing corrupted dump file
+        if [ $i -eq $RETRIES ]; then
+            echo "Failed all attempts, exiting."
+            exit 1
+        fi
+    fi
+done
 
-if [ -n "$MONGO_QUERY" ]; then
-    DUMP_CMD+=" --query=\"$MONGO_QUERY\""
-fi
-
-# Running the mongodump command
-eval $DUMP_CMD
-
-# Configure AWS CLI with S3 or MinIO endpoint
-if [ -n "$S3_ENDPOINT" ]; then
-    aws configure set default.s3.endpoint_url "$S3_ENDPOINT"
+# Configure AWS CLI with S3 or MinIO endpoint and optionally access keys
+aws configure set default.s3.endpoint_url "$S3_OR_MINIO_ENDPOINT"
+if [ -n "$ACCESS_KEY" ] && [ -n "$SECRET_KEY" ]; then
+    aws configure set aws_access_key_id "$ACCESS_KEY"
+    aws configure set aws_secret_access_key "$SECRET_KEY"
 else
-    aws configure set default.s3.endpoint_url "$MINIO_ENDPOINT"
+    NO_SIGN_REQUEST="--no-sign-request"
 fi
 
 # Upload to the given STORAGE_PATH
-aws s3 cp /dump/archive s3://$STORAGE_PATH
+aws s3 cp $NO_SIGN_REQUEST "$FILENAME" s3://$STORAGE_PATH --endpoint-url "$S3_OR_MINIO_ENDPOINT"
